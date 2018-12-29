446dab04d95d6bbab2b789645127eb0504b0bc46
Vlad <gribennikov.vlad@gmail.com>
2018-12-29 11:36:44 +0200

    Add port and npm start for heroku

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const {PORT = 3000} = process.env;
 
 app.get("/myname/", (req, res, next) => {
     res.json({
@@ -7,6 +8,6 @@ app.get("/myname/", (req, res, next) => {
     })
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("SERVER IS STARTED");
 })
